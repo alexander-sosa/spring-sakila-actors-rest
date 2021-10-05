9b9e5327c96ce82a551c07c3ff4b88ea4b5bf67c
Alexander Sosa <junior.sosa@ucb.edu.bo>
2021-10-04 22:13:29 -0400

    first project commit

---
+++ src/main/java/bo/edu/ucb/ingsoft/actors/ActorsApplication.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.ingsoft.actors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActorsApplication.class, args);
+	}
+
+}
